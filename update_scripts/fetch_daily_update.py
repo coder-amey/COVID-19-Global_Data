494f562537918e30108fd365e4936033aed47a77
Amey Noolkar <fb4amey@gmail.com>
2020-06-01 22:49:48 +0530

    Added script for retrieving daily updates.

--- update_scripts/fetch_daily_update.py
+++ update_scripts/fetch_daily_update.py
@@ -1,35 +1,65 @@
-'''This script scrapes the home page of the MoHFW, Govt. of India, for updates on COVID-19 data and stores the latest tally in a CSV file with the corresponding date in the filename. It also updates the time-series dataset and regenerates the corresponding CSV file.'''
+'''This script retrieves daily updates on COVID-19 data from the repository of the CSSE, JHU [https://github.com/CSSEGISandData] and stores the latest tally in a CSV file with the corresponding date in the filename. It also updates the time-series dataset and regenerates the corresponding CSV file.'''
 
 import pandas as data
-import numpy as maths
 import os.path
+from datetime import timedelta
 from datetime import datetime
 
-rows = 35	#The number of rows to be fetched from the table (this may change).
 base_dir = os.path.join(os.path.dirname(__file__), "../")		#Obtain the path to the base directory for absosulte addressing.
 date = datetime.now().strftime("%d-%m-%Y") 	#Date of update.
+day_delta = timedelta(days = 1)			#Add time delta to fix timezone mismatch.
 
-#Scrape the HTML tables, load the last table, remove unwanted columns and rename and reformat the columns. Add the "National Total" row.
-tables = data.read_html("https://www.mohfw.gov.in/")
-updated_tally = tables[-1][:rows].drop(["S. No.", "Active Cases*"], axis = 1)
-updated_tally = updated_tally.rename(columns = {"Name of State / UT": "Region", "Total Confirmed cases*": "Confirmed", "Cured/Discharged/Migrated*": "Recovered/Migrated", "Deaths**": "Deceased"})
+def generate_dataset(record):
+	'''Generate a dataframe from a record of tallies for a particular date.'''
+	df = data.DataFrame({"Confirmed": record[0], "Recovered/Migrated": record[1], "Deceased": record[2]}).reset_index().rename(columns = {"index": "Region"})
+	df = df[df.Confirmed != 0]		#Ignore regions with no cases.
+	df = df.append(df.sum(numeric_only = True), ignore_index = True)
+	df.iloc[-1, 0] = "Global Total"
+	return(df)
 
-updated_tally = updated_tally.astype({"Confirmed": int, "Recovered/Migrated": int, "Deceased": int})
-updated_tally = updated_tally.append(updated_tally.sum(numeric_only = True), ignore_index = True)
-updated_tally.iloc[-1, 0] = "National Total"
+if __name__ == "__main__":
+	#Load historical data.
+	raw_CNF = data.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+	raw_RCV = data.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
+	raw_DCS = data.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 
-#Correct the errors in the table.
-updated_tally.loc[updated_tally.Region == "Telengana", "Region"] = "Telangana"		#Correct the spelling of Telangana.
-updated_tally.loc[updated_tally.Region == "Dadar Nagar Haveli", "Region"] = "Dadra and Nagar Haveli"		#Correct the spelling of D&NH.
+	#Initialize the time-series.
+	record = dict()
+	time_series = data.read_csv(base_dir + "time-series/Global_aggregated.csv")		#Load the time-series.
+	time_series = time_series[time_series["Date"] != date]	#Discard other updates made today.
 
-#Store the dataset to a CSV file.
-updated_tally.to_csv(base_dir + "datasets/India_regional_aggregated_{}.csv".format(date), index = False)
+	#Clean and transform the data.
+	raw_CNF = raw_CNF[["Country/Region", raw_CNF.keys()[-1]]]
+	raw_CNF = raw_CNF.rename(columns = {"Country/Region": "Region"}).groupby(["Region"]).sum().T
+	raw_CNF.index = data.to_datetime(raw_CNF.index)
 
-#Insert a "Date" column to the data frame and merge it with the time-series.
-updated_tally.insert(1, "Date", date)
-time_series = data.read_csv(base_dir + "time-series/India_regional_aggregated.csv")		#Load the time-series.
-time_series = time_series[time_series["Date"] != date]	#Discard other updates made today.
-time_series = data.concat([time_series, updated_tally])
+	raw_RCV = raw_RCV[["Country/Region", raw_RCV.keys()[-1]]]
+	raw_RCV = raw_RCV.rename(columns = {"Country/Region": "Region"}).groupby(["Region"]).sum().T
+	raw_RCV.index = data.to_datetime(raw_RCV.index)
 
-#Rewrite the updated time-series to its CSV file.
-time_series.to_csv(base_dir + "time-series/India_regional_aggregated.csv", index = False)
+	raw_DCS = raw_DCS[["Country/Region", raw_DCS.keys()[-1]]]
+	raw_DCS = raw_DCS.rename(columns = {"Country/Region": "Region"}).groupby(["Region"]).sum().T
+	raw_DCS.index = data.to_datetime(raw_DCS.index)
+
+	#Implement spelling corrections here:
+	rename_dict = {'Taiwan*': 'Taiwan', 'Korea, South': 'South Korea', 'US': 'United States of America', 'Czechia': 'Czech Republic', 'Holy See': 'Vatican City'}
+	raw_CNF = raw_CNF.rename(columns = rename_dict)
+	raw_RCV = raw_RCV.rename(columns = rename_dict)
+	raw_DCS = raw_DCS.rename(columns = rename_dict)
+
+	#Generate a dataframe of date-wise aggregated data.
+	assert (date ==(raw_CNF.index + day_delta)),"Date mismatch. New data is not contiguous with existing entries."
+
+	#Prepare values.
+	tally = [table.loc[date - day_delta] for table in [raw_CNF, raw_RCV, raw_DCS]]
+	#Generate a daily record for the particular date.
+	updated_tally = generate_dataset(tally)
+
+	updated_tally.to_csv(base_dir + "datasets/Global_aggregated_{}.csv".format(date.strftime('%d-%m-%Y')), index = False)
+		
+	#Add a date column and assimilate the records into a time_series dataframe with historical data.
+	updated_tally.insert(0, "Date", date.strftime('%d-%m-%Y'))	
+	time_series = time_series.append(daily_record, ignore_index = True)	
+
+	#Write the updated time-series to its CSV file.
+	time_series.to_csv(base_dir + "time-series/Global_aggregated.csv", index = False)
